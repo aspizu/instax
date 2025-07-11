8d2e8f4eeb40ef6a766b4ca43b6afa2d655b802d
aspizu <aspizu@protonmail.com>
2025-07-11 22:53:16 +0530

    fix: use useId hook to create html ids

--- src/components/radio-card.tsx
+++ src/components/radio-card.tsx
@@ -1,5 +1,6 @@
 import {Label} from "@/components/ui/label"
 import {RadioGroupItem} from "@/components/ui/radio-group"
+import {useId} from "react"
 
 export interface RadioCardProps {
     value: string
@@ -8,10 +9,11 @@ export interface RadioCardProps {
 }
 
 export function RadioCard({value, name, description}: RadioCardProps) {
+    const id = useId()
     return (
         <div className='hover:bg-input/50 [&:has([aria-checked="true"])]:bg-input/50 flex items-center gap-2 rounded-lg p-2 transition-colors'>
-            <RadioGroupItem value={value} id={value} />
-            <Label htmlFor={value} className="flex flex-col items-start gap-1">
+            <RadioGroupItem value={value} id={id} />
+            <Label htmlFor={id} className="flex flex-col items-start gap-1">
                 {name}
                 <span className="text-muted-foreground text-xs">{description}</span>
             </Label>
